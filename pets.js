1adeb85bd2d98652a8ae237bd9e9ce5db11915c8
sanjeewna <sanjeewnadevi93@gmail.com>
2023-10-09 17:34:22 +0530

    Solved lab 3 week 2

---
+++ pets.js
@@ -0,0 +1,24 @@
+// pets.js
+
+const petsData = [
+    {
+      id: 1,
+      name: 'Buddy',
+      species: 'Dog',
+      age: 3,
+    },
+    {
+      id: 2,
+      name: 'Whiskers',
+      species: 'Cat',
+      age: 2,
+    },
+    {
+      id: 3,
+      name: 'Rex',
+      species: 'Dog',
+      age: 5,
+    },
+  ];
+  
+  module.exports = petsData;
